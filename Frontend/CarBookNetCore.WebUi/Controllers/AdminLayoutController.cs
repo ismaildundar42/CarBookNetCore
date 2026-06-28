2440182d651db2ab5283afd1eb3daa2561564eb0
İsmail Dündar <ismail.dndr42@gmail.com>
2026-06-28 13:23:29 +0300

    Admin paneline giriş yapıldı - partial view

---
+++ Frontend/CarBookNetCore.WebUi/Controllers/AdminLayoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookNetCore.WebUi.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AdminSidebarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
