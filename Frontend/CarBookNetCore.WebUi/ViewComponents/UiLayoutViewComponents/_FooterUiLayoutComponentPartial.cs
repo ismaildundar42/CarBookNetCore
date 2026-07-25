9a14485edc392eba0c3aedc34941479078c8d728
İsmail Dündar <ismail.dndr42@gmail.com>
2026-07-25 16:24:05 +0300

    Footer işlemleri dinamikleştirildi ve sweet alert eklendi

--- Frontend/CarBookNetCore.WebUi/ViewComponents/UiLayoutViewComponents/_FooterUiLayoutComponentPartial.cs
+++ Frontend/CarBookNetCore.WebUi/ViewComponents/UiLayoutViewComponents/_FooterUiLayoutComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookNetCore.Dtos.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace CarBookNetCore.WebUi.ViewComponents.UiLayoutViewComponents
 {
     public class _FooterUiLayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUiLayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7187/api/FooterAddresses");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
